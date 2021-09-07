3c9c6bd7ce61d83e696935bb4a72157c1162afef
Rick Martinez <61433776+iDevRM@users.noreply.github.com>
2021-09-07 13:53:12 -0400

    Initial Commit

---
+++ Reddit Feed/Reddit Feed/Reddit_FeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reddit_FeedApp.swift
+//  Reddit Feed
+//
+//  Created by Rick Martinez on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Reddit_FeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Reddit Feed/Reddit FeedTests/Reddit_FeedTests.swift
@@ -0,0 +1,33 @@
+//
+//  Reddit_FeedTests.swift
+//  Reddit FeedTests
+//
+//  Created by Rick Martinez on 9/7/21.
+//
+
+import XCTest
+@testable import Reddit_Feed
+
+class Reddit_FeedTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
